7aaecc81325e0ff52c031c0687e9c7a742f1e947
unknown <a17749680605@outlook.com>
2026-01-29 10:06:30 +0800

    made same changes to __init__.py

--- __init__.py
+++ __init__.py
@@ -27,7 +27,7 @@ async def main(bot: Bot, event: Event):
         if text != "帮助":
             try:
                 async with aiofiles.open(os.path.dirname(os.path.abspath(__file__)) +"/utils/info.json",'r') as f:
-                    content = f.read()
+                    content = await f.read()
                     info = json.loads(content)
                 for key,val in info.items():
                     if text in val:
